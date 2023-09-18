2063cb7684abd597f64d48c9dd0e78082c9f5d96
marfu <manfy1993@163.com>
2023-09-18 15:23:27 +0800

    心跳协议

--- cpp/Common/CommonCmd.pb.h
+++ cpp/Common/CommonCmd.pb.h
@@ -186,12 +186,14 @@ enum EMsgIDLogin : int {
   MsgIDShop_QueryTrumpetPriceResp = 99,
   MsgIDLogin_QueryServerTime = 100,
   MsgIDLogin_QueryServerTimeResp = 101,
+  MsgIDLogin_Heart = 102,
+  MsgIDLogin_HeartResp = 103,
   EMsgIDLogin_INT_MIN_SENTINEL_DO_NOT_USE_ = std::numeric_limits<::PROTOBUF_NAMESPACE_ID::int32>::min(),
   EMsgIDLogin_INT_MAX_SENTINEL_DO_NOT_USE_ = std::numeric_limits<::PROTOBUF_NAMESPACE_ID::int32>::max()
 };
 bool EMsgIDLogin_IsValid(int value);
 constexpr EMsgIDLogin EMsgIDLogin_MIN = MsgIDLogin_Null;
-constexpr EMsgIDLogin EMsgIDLogin_MAX = MsgIDLogin_QueryServerTimeResp;
+constexpr EMsgIDLogin EMsgIDLogin_MAX = MsgIDLogin_HeartResp;
 constexpr int EMsgIDLogin_ARRAYSIZE = EMsgIDLogin_MAX + 1;
 
 const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* EMsgIDLogin_descriptor();
