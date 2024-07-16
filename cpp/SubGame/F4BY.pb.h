c18e48e4c9876fd9247750415957a96d278e74a2
marfu <manfy1993@163.com>
2024-07-16 14:45:41 +0800

    pb

--- cpp/SubGame/F4BY.pb.h
+++ cpp/SubGame/F4BY.pb.h
@@ -15350,6 +15350,8 @@ class SCUserPoolDraw PROTOBUF_FINAL :
     kAwardFieldNumber = 3,
     kAwardCntFieldNumber = 4,
     kChairIdxFieldNumber = 1,
+    kUpgradecostFieldNumber = 5,
+    kIndexFieldNumber = 6,
   };
   // repeated .F4BY.Object2 goods = 2;
   int goods_size() const;
@@ -15414,6 +15416,24 @@ class SCUserPoolDraw PROTOBUF_FINAL :
   void _internal_set_chair_idx(::PROTOBUF_NAMESPACE_ID::int32 value);
   public:
 
+  // int32 upgradecost = 5;
+  void clear_upgradecost();
+  ::PROTOBUF_NAMESPACE_ID::int32 upgradecost() const;
+  void set_upgradecost(::PROTOBUF_NAMESPACE_ID::int32 value);
+  private:
+  ::PROTOBUF_NAMESPACE_ID::int32 _internal_upgradecost() const;
+  void _internal_set_upgradecost(::PROTOBUF_NAMESPACE_ID::int32 value);
+  public:
+
+  // int32 index = 6;
+  void clear_index();
+  ::PROTOBUF_NAMESPACE_ID::int32 index() const;
+  void set_index(::PROTOBUF_NAMESPACE_ID::int32 value);
+  private:
+  ::PROTOBUF_NAMESPACE_ID::int32 _internal_index() const;
+  void _internal_set_index(::PROTOBUF_NAMESPACE_ID::int32 value);
+  public:
+
   // @@protoc_insertion_point(class_scope:F4BY.SCUserPoolDraw)
  private:
   class _Internal;
@@ -15425,6 +15445,8 @@ class SCUserPoolDraw PROTOBUF_FINAL :
   ::F4BY::Object2* award_;
   ::F4BY::Object* award_cnt_;
   ::PROTOBUF_NAMESPACE_ID::int32 chair_idx_;
+  ::PROTOBUF_NAMESPACE_ID::int32 upgradecost_;
+  ::PROTOBUF_NAMESPACE_ID::int32 index_;
   mutable ::PROTOBUF_NAMESPACE_ID::internal::CachedSize _cached_size_;
   friend struct ::TableStruct_F4BY_2eproto;
 };
@@ -47556,6 +47578,46 @@ inline void SCUserPoolDraw::set_allocated_award_cnt(::F4BY::Object* award_cnt) {
   }
   award_cnt_ = award_cnt;
   // @@protoc_insertion_point(field_set_allocated:F4BY.SCUserPoolDraw.award_cnt)
+}
+
+// int32 upgradecost = 5;
+inline void SCUserPoolDraw::clear_upgradecost() {
+  upgradecost_ = 0;
+}
+inline ::PROTOBUF_NAMESPACE_ID::int32 SCUserPoolDraw::_internal_upgradecost() const {
+  return upgradecost_;
+}
+inline ::PROTOBUF_NAMESPACE_ID::int32 SCUserPoolDraw::upgradecost() const {
+  // @@protoc_insertion_point(field_get:F4BY.SCUserPoolDraw.upgradecost)
+  return _internal_upgradecost();
+}
+inline void SCUserPoolDraw::_internal_set_upgradecost(::PROTOBUF_NAMESPACE_ID::int32 value) {
+  
+  upgradecost_ = value;
+}
+inline void SCUserPoolDraw::set_upgradecost(::PROTOBUF_NAMESPACE_ID::int32 value) {
+  _internal_set_upgradecost(value);
+  // @@protoc_insertion_point(field_set:F4BY.SCUserPoolDraw.upgradecost)
+}
+
+// int32 index = 6;
+inline void SCUserPoolDraw::clear_index() {
+  index_ = 0;
+}
+inline ::PROTOBUF_NAMESPACE_ID::int32 SCUserPoolDraw::_internal_index() const {
+  return index_;
+}
+inline ::PROTOBUF_NAMESPACE_ID::int32 SCUserPoolDraw::index() const {
+  // @@protoc_insertion_point(field_get:F4BY.SCUserPoolDraw.index)
+  return _internal_index();
+}
+inline void SCUserPoolDraw::_internal_set_index(::PROTOBUF_NAMESPACE_ID::int32 value) {
+  
+  index_ = value;
+}
+inline void SCUserPoolDraw::set_index(::PROTOBUF_NAMESPACE_ID::int32 value) {
+  _internal_set_index(value);
+  // @@protoc_insertion_point(field_set:F4BY.SCUserPoolDraw.index)
 }
 
 // -------------------------------------------------------------------
